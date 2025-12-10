b1b9c18ef89ada85b5d188a57552d8966aed4b4c
g0vindgaur1133-sys <g0vindgaur1133@gmail.com>
2025-12-10 10:34:00 +0530

    operator in sql

--- basic.sql
+++ basic.sql
@@ -32,8 +32,67 @@ FROM city;
 -- alias ( nickname)
 select *, population+12+12-4-5-12-4 as 'newpopulation' from country;
 
--- what is database
--- what is filesysten and how it is differenet from dbms
--- type of DBMS and the name for each of one
--- DBMS vs RDBMS
--- where cluase
+describe city;
+-- where clause we will filter the data
+select * from city where district='Zuid-Holland';
+
+select * from city where countrycode='ARG'; -- filter city for ARG country code
+
+-- 6 more than 6 lakh
+select * from city where population>600000;
+
+-- <> != ( not equals to )
+select * from city where countrycode <> 'ARG'; -- filter city for ARG country code
+
+-- in operator
+select * from country where continent='North America';
+
+select * from country where continent='Europe';
+
+
+-- getting countries in below continents
+select * from country where continent in ('North America','Europe','Asia');
+
+-- indepyear
+select name, continent from country where indepyear in(1901, 1960);
+
+-- range of values
+select name, continent, indepyear from country where indepyear between 1901 and 1960;
+-- not operator use to block the range of specific value
+select name, continent, indepyear from country where indepyear not between 1901 and 1960;
+-- between range of values
+-- in -> specific value me search krna
+
+-- ques
+select name, continent, population, gnp from country where population not between 5000 and 200000;
+
+select name, region from country where region not in ('Middle East');
+
+select * from country where lifeexpectancy in (38.3, 66.4);
+
+select name, population, population*0.1 + population as population from country;
+
+-- like operator
+-- like => pattern ko seaarch krna
+select name, continent from country where continent='Asia';
+
+-- to meet with pattern
+-- special character is called wildcard character
+-- % [we are finding zero or more character]
+-- Asian Asia%
+select name, continent from country where name like '%ad%';
+
+-- _ ( underscore) => 1 character
+
+select name, continent from country where name like 'Ir__'
+
+
+
+
+
+
+
+
+
+
+
